515bbf1b45989da0a678e4c1ff8388d1b0a2ad8c
Andreas Saltsberg <andreas19961996@gmail.com>
2016-08-03 20:15:28 +0300

    Update

---
+++ app/src/main/java/com/nutikorv/andreas/nutikorvalpha/Parameters/GlobalParameters.java
@@ -0,0 +1,15 @@
+package com.nutikorv.andreas.nutikorvalpha.Parameters;
+
+import com.nutikorv.andreas.nutikorvalpha.Objects.Basket;
+import com.nutikorv.andreas.nutikorvalpha.Objects.ReadProducts;
+
+/**
+ * Created by ANDREAS on 12.07.2016.
+ */
+public class GlobalParameters {
+
+    public static Basket b = new Basket("LUL");
+
+    public static ReadProducts r;
+
+}
